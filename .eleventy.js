20f2efb56100faadc260abb48d7e16fed1864774
baiway <bailey.cook@sky.com>
2023-05-26 21:38:01 +0100

    clean file

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,6 @@ const { DateTime } = require("luxon");
 const fs = require("fs");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.setNunjucksEnvironmentOptions({
-    throwOnUndefined: true,
-  });
-
   //Pass through files
   eleventyConfig.addPassthroughCopy("./src/styles.css");  
   eleventyConfig.addPassthroughCopy("./src/markdown.css");
@@ -16,8 +12,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   // Get file creation and last modification dates
-  // usage: {{ page | fileCreationDate | postDate }}
-  // usage: {{ page | fileLastModifiedDate | postDate }}
+  //   usage: {{ page | fileCreationDate | postDate }}
+  //   usage: {{ page | fileLastModifiedDate | postDate }}
   eleventyConfig.addFilter("fileCreationDate", function (post) {
     const stats = fs.statSync(post.inputPath);
     return stats.birthtime;
@@ -27,12 +23,10 @@ module.exports = function(eleventyConfig) {
     return stats.mtime;
   });  
 
-
   // reformat post.date to from e.g. 2023-05-20 to 20 May 2023.
-  // useage: 
-  //  {{ date | postDate }} in the file where the date is declared, as in 
-  //  src/_includes/blog-post.njk template,  or 
-  // {{ post.Date | postDate }} when looping over files, as in src/blog/index.html 
+  //   useage: {{ date | postDate }} in the file where the date is 
+  // declared, as in src/_includes/blog-post.njk template,  or 
+  //   {{ post.Date | postDate }} when looping over files, as in src/blog/index.html 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).setLocale("en-gb").toLocaleString(DateTime.DATE_FULL);
   });
